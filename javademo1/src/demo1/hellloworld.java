881067909eab0aa0f1711d5d2c4caf829678e843
admin <1985478118@qq.com>
2020-11-20 19:04:39 +0800

    IDEA提交

--- javademo1/src/demo1/hellloworld.java
+++ javademo1/src/demo1/hellloworld.java
@@ -3,6 +3,8 @@ package demo1;
 public class hellloworld {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+		System.out.println("idea 提交1");
+		System.out.println("idea 提交2");
 	}
 
 }
